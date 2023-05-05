367a7444cff79b82e99bf868fd2f881bc622f19c
sawan <dsawan.office8@gmail.com>
2023-05-05 16:28:15 +0530

    added code for persisting the data.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterSlice from "./features/counter/counterSlice";
+import { persistReducer, persistStore } from "redux-persist";
+// import storage from "redux-persist/lib/storage"; // defaults to localStorage
+import storage from "redux-persist-indexeddb-storage";
+
+const persistConfig = {
+  key: "root",
+  storage: storage("database"),
+};
+
+const persistedReducer = persistReducer(persistConfig, counterSlice);
 
 export const store = configureStore({
   reducer: {
-    counter: counterSlice,
+    counter: persistedReducer,
   },
+  middleware: (getDefaultMiddleware) => [
+    ...getDefaultMiddleware({ thunk: false, serializableCheck: false }),
+  ],
 });
+
+export const persistor = persistStore(store);
